6d5eee0db0e0de45f4389b24ae4ad3cf01997be1
Marina <marina.kotceva78@gmail.com>
2025-03-16 14:17:29 +0100

    login, register

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -5,9 +5,10 @@ export default function Footer() {
     return (
         <footer>
             <div className="footer-left">
-                <p>1234 Street Name, City, Country</p>
+                <p>7524 Salem Rd, Wonder Lake</p>
+                <p>60097 Ilinois, USA</p>
                 <p>+1 234 567 890</p>
-                <p>contact@example.com</p>
+                <p>contact@myplaceyourplace.com</p>
             </div>
             <div className="footer-right">
                 <a href="#"><FaFacebookF /></a>
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,36 +1,34 @@
 import { Link, useNavigate } from "react-router";
 
-export default function Login({
-    onLogin,
-}) {
+export default function Login({ onLogin }) {
     const navigate = useNavigate();
 
     const loginAction = (formData) => {
         const email = formData.get('email');
-
         onLogin(email);
-
         navigate('/places');
     }
 
     return (
-        <section id="login-page" className="auth">
-            <form id="login" action={loginAction}>
+        <div className="auth-container">
+            <section id="login-page" className="auth">
+                <form id="login" action={loginAction}>
+                    <div className="container">
+                        <div className="brand-logo"></div>
+                        <h1>Login</h1>
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="email" placeholder="your@email.com" />
 
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Login</h1>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+                        <label htmlFor="login-pass">Password:</label>
+                        <input type="password" id="login-password" name="password" />
 
-                    <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
-                    <input type="submit" className="btn submit" value="Login" />
-                    <p className="field">
-                        <span>If you don't have profile click <Link to="/register">here</Link></span>
-                    </p>
-                </div>
-            </form>
-        </section>
+                        <input type="submit" className="btn submit" value="Login" />
+                        <p className="field">
+                            <span>If you don't have profile click <Link to="/register">here</Link></span>
+                        </p>
+                    </div>
+                </form>
+            </section>
+        </div>
     );
 }
